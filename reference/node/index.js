4b56e93c13e493d2227a8bbe4d2faea9ded56325
Willard Torres <willard.torres1008@gmail.com>
2017-06-14 16:49:52 +0800

    Move message construction to prepare_message

--- reference/node/index.js
+++ reference/node/index.js
@@ -4,11 +4,14 @@ const protobuf = require('protobufjs');
 const sodium = require('libsodium-wrappers');
 const NONCE_SIZE = 12;
 const MAC_SIZE = 16;
+const OBDI_MARKER = [79, 66, 68, 73];
+const OBDI_VERSION = 0;
 
 var socket = dgram.createSocket('udp4');
 
 var obdi;
 var crypto;
+var client_id = 1;
 var sign_pk, sign_sk, server_sign_pk;
 var client_pk_path = process.argv[2];
 var client_sk_path = process.argv[3];
@@ -65,17 +68,64 @@ fs.readFile(server_pk_path, function(err, data) {
 });
 
 var ClientMessageHeader = function() {
-	this.marker = [79, 66, 68, 73];
-	this.version = 0;
+	this.marker = OBDI_MARKER; 
+	this.version = OBDI_VERSION;
 	this.message_type = 0;
 	this.payload_size = 0;
 	this.vessel_id = 0;
 	this.payload_ad = null;	
 }
 
+var MessageType;
+var Notice, Ping, Ack, CryptoError, LocationUpdate, TripInfoUpdateStatus, ChangeSettings, Error, ETAUpdate, TripInfoUpdate;
+
+function prepare_message(message, type) {
+	var message_bytes = type.encode(message).finish();
+	var header_bytes = Buffer.alloc(16);
+	header_bytes.writeUInt8(OBDI_MARKER[0], 0);
+	header_bytes.writeUInt8(OBDI_MARKER[1], 1);
+	header_bytes.writeUInt8(OBDI_MARKER[2], 2);
+	header_bytes.writeUInt8(OBDI_MARKER[3], 3);
+	header_bytes.writeUInt8(OBDI_VERSION, 4);
+	header_bytes.writeUInt8(MessageType[type.name], 5);
+	header_bytes.writeUInt16LE(message_bytes.length, 6);
+	header_bytes.writeUIntLE(client_id, 8, 8);
+
+	var res = crypto.encrypt_payload(header_bytes, message_bytes);
+	return Buffer.concat([header_bytes, res.nonce_bytes, res.mac_bytes, res.encrypted_bytes]);
+}
+
 protobuf.load('../../obdi.proto', function(err, root) {
 	if(err) throw err;
 	obdi = root;
+
+	Notice = obdi.lookupType('obdi.Notice');
+	Ping = obdi.lookupType('obdi.Ping');
+	Ack = obdi.lookupType('obdi.Ack');
+	CryptoError = obdi.lookupType('obdi.CryptoError');
+	LocationUpdate = obdi.lookupType('obdi.LocationUpdate');
+	TripInfoUpdateStatus = obdi.lookupType('obdi.TripInfoUpdateStatus');
+	ChangeSettings = obdi.lookupType('obdi.ChangeSettings');
+	Error = obdi.lookupType('obdi.Error');
+	ETAUpdate = obdi.lookupType('obdi.ETAUpdate');
+	TripInfoUpdate = obdi.lookupType('obdi.TripInfoUpdate');
+
+	MessageType = {
+		Notice: 0,
+		Ping: 2,
+		Ack: 3,
+
+		CryptoErrpr: 10,
+		
+		LocationUpdate: 20,
+		TripInfoUpdateStatus: 21,
+
+		ChangeSettings: 50,
+		Error: 51,
+		ETAUpdate: 52,
+		TripInfoUpdate: 53
+	};
+
 	if(initialized()) main();
 });
 
@@ -89,26 +139,7 @@ function main() {
 
 	var now = parseInt(Date.now() / 1000, 10);
 	var ping = Ping.create({messageId: 12|0, timeGenerated: {seconds: now, nanos: 0|0 }});
-	var ping_bytes = Ping.encode(ping).finish();
-
-	var header = new ClientMessageHeader();
-	header.version = 0;
-	header.message_type = 2;
-	header.payload_size = ping_bytes.length;
-	header.vessel_id = 1;
-	var nonce = sodium.randombytes_buf(NONCE_SIZE);
-	var header_bytes = Buffer.alloc(16);
-	header_bytes.writeUInt8(header.marker[0], 0);
-	header_bytes.writeUInt8(header.marker[1], 1);
-	header_bytes.writeUInt8(header.marker[2], 2);
-	header_bytes.writeUInt8(header.marker[3], 3);
-	header_bytes.writeUInt8(header.version, 4);
-	header_bytes.writeUInt8(header.message_type, 5);
-	header_bytes.writeUInt16LE(header.payload_size, 6);
-	header_bytes.writeUIntLE(header.vessel_id, 8, 8);
-
-	var res = crypto.encrypt_payload(header_bytes, ping_bytes);
-	var send_buffer = Buffer.concat([header_bytes, res.nonce_bytes, res.mac_bytes, res.encrypted_bytes]);
+	var send_buffer = prepare_message(ping, Ping);
 
 	socket.send(send_buffer, 0, send_buffer.length, 1234, '127.0.0.1', function(err, bytes) {
 		if(err) throw err;
