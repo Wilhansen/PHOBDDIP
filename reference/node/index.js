8f0e80de2fbf1438740e6bf5fb59f8979c9b7cca
Willard Torres <willard.torres1008@gmail.com>
2017-06-15 13:31:57 +0800

    Add classes for headers, functionality for recieving messages

--- reference/node/index.js
+++ reference/node/index.js
@@ -26,6 +26,41 @@ var ClientCrypto = function(sign_pk, sign_sk, server_sign_pk) {
 	this.derive_keys();
 };
 
+var ClientMessageHeader = function() {
+	this.marker = OBDI_MARKER;
+	this.version = OBDI_VERSION;
+	this.payload_size = 0;
+	this.message_type = 0;
+	this.vessel_id = 0;
+};
+
+ClientMessageHeader.prototype.toBuffer = function() {
+	var buffer = Buffer.alloc(16);
+	buffer.writeUInt8(this.marker[0], 0);
+	buffer.writeUInt8(this.marker[1], 1);
+	buffer.writeUInt8(this.marker[2], 2);
+	buffer.writeUInt8(this.marker[3], 3);	
+	buffer.writeUInt8(this.version, 4);
+	buffer.writeUInt8(MessageType[this.message_type.name], 5);
+	buffer.writeUInt16LE(this.payload_size, 6);
+	buffer.writeUIntLE(this.vessel_id, 8, 8);
+	return buffer;
+};
+
+var ServerMessageHeader = function() {
+	this.marker = OBDI_MARKER;
+	this.version = OBDI_VERSION;
+	this.payload_size = 0;
+	this.message_type = 0;
+};
+
+ServerMessageHeader.prototype.fromBuffer = function(buffer) {
+	this.marker = buffer.slice(0, 4);
+	this.version = buffer.readUInt8(4);
+	this.payload_size = buffer.readUInt16LE(5);
+	this.message_type = buffer.readUInt8(7);
+};
+
 ClientCrypto.prototype.derive_keys = function() {
 	this.client_pk = sodium.crypto_sign_ed25519_pk_to_curve25519(this.sign_pk);
 	this.client_sk = sodium.crypto_sign_ed25519_sk_to_curve25519(this.sign_sk);
@@ -45,6 +80,18 @@ ClientCrypto.prototype.encrypt_payload = function(header, payload) {
 		nonce_bytes: Buffer.from(nonce.buffer),
 		mac_bytes: Buffer.from(res.mac.buffer)
 	};
+};
+
+ClientCrypto.prototype.decrypt_payload = function(header, payload) {
+	var header_marker = header.slice(0, 4);
+	var header_version = header.readUInt8(4);
+	var header_payload_size = header.readUInt16LE(5);
+	var header_message_type = header.readUInt8(7);
+	var header_nonce = header.slice(8, 20);
+	var header_mac = header.slice(20, 36);
+
+	var res = sodium.crypto_aead_chacha20poly1305_ietf_decrypt_detached(null, payload, header_mac, header.slice(0, 8), header_nonce, this.client_rx);
+	console.log(res);
 };
 
 function initialized() {
@@ -72,14 +119,13 @@ fs.readFile(server_pk_path, function(err, data) {
 function prepare_message(message, type) {
 	var message_bytes = type.encode(message).finish();
 	var header_bytes = Buffer.alloc(16);
-	header_bytes.writeUInt8(OBDI_MARKER[0], 0);
-	header_bytes.writeUInt8(OBDI_MARKER[1], 1);
-	header_bytes.writeUInt8(OBDI_MARKER[2], 2);
-	header_bytes.writeUInt8(OBDI_MARKER[3], 3);
-	header_bytes.writeUInt8(OBDI_VERSION, 4);
-	header_bytes.writeUInt8(MessageType[type.name], 5);
-	header_bytes.writeUInt16LE(message_bytes.length, 6);
-	header_bytes.writeUIntLE(client_id, 8, 8);
+	var client_header = new ClientMessageHeader();
+	client_header.marker = OBDI_MARKER;
+	client_header.version = OBDI_VERSION;
+	client_header.message_type = type;
+	client_header.payload_size = message_bytes.length;
+	client_header.vessel_id = client_id;
+	var header_bytes = client_header.toBuffer();
 
 	var res = crypto.encrypt_payload(header_bytes, message_bytes);
 	return Buffer.concat([header_bytes, res.nonce_bytes, res.mac_bytes, res.encrypted_bytes]);
@@ -105,7 +151,7 @@ protobuf.load('../../obdi.proto', function(err, root) {
 		Ping: 2,
 		Ack: 3,
 
-		CryptoErrpr: 10,
+		CryptoError: 10,
 		
 		LocationUpdate: 20,
 		TripInfoUpdateStatus: 21,
@@ -128,11 +174,17 @@ function main() {
 	var Severity = obdi.lookup('obdi.Severity');
 
 	var now = parseInt(Date.now() / 1000, 10);
-	var ping = Ping.create({messageId: 12|0, timeGenerated: {seconds: now, nanos: 0|0 }});
+	var ping = Ping.create({messageId: 1|0, timeGenerated: {seconds: now, nanos: 0|0 }});
 	var send_buffer = prepare_message(ping, Ping);
 
 	socket.send(send_buffer, 0, send_buffer.length, 1234, '127.0.0.1', function(err, bytes) {
 		if(err) throw err;
-		socket.close();
+	});
+
+	socket.on('message', function(message, remote) {
+		var header_bytes = message.slice(0, 36);
+		var payload_bytes = message.slice(36);
+		var decrypted = crypto.decrypt_payload(header_bytes, payload_bytes);
+		
 	});
 }
